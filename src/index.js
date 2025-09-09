dbdd46fc024470a8ae995159cc75ef0100d2b85f
ikram <ikramlahmouri19@gmail.com>
2025-09-09 11:09:41 +0200

    WIP: mes modifs avant merge scraper

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
 
+import sessionsRoutes from "./routes/sessions.js";
+import musicsRoutes from "./routes/musics.js";
+import votesRoutes from "./routes/votes.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Test route
+//Routes
+app.use("/sessions", sessionsRoutes);
+app.use("/musics", musicsRoutes);
+app.use("/votes", votesRoutes);
+
 app.get("/", (req, res) => {
   res.json({ message: "Backend voting app is running 🚀" });
 });
---
+++ src/routes/musics.js
@@ -0,0 +1,35 @@
+import express from "express";
+import prisma from "../config/db.js";
+
+const router = express.Router();
+
+// Ajouter une musique
+router.post("/", async (req, res) => {
+  const { sessionId, title, artist } = req.body;
+
+  try {
+    const music = await prisma.music.create({
+      data: { sessionId: Number(sessionId), title, artist },
+    });
+    res.json(music);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur ajout musique", details: err.message });
+  }
+});
+
+// Récupérer musiques d'une session
+router.get("/:sessionId", async (req, res) => {
+  const { sessionId } = req.params;
+
+  try {
+    const musics = await prisma.music.findMany({
+      where: { sessionId: Number(sessionId) },
+      include: { votes: true },
+    });
+    res.json(musics);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur récupération musiques", details: err.message });
+  }
+});
+
+export default router;
